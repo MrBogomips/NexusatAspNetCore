420aab6829f6c4c92190204dbdf9d06b4ac8192e
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2018-03-21 15:42:31 +0100

    Solutions

---
+++ Nexusat.AspNetCore.SampleRestApi/Models/HelloWorldResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nexusat.AspNetCore.SampleRestApi.Models
+{
+    public class HelloWorldResponse
+    {
+        public string Greetings;
+    }
+}
