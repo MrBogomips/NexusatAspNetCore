2bd33b2ab161f0aa46c117566bb45959200c2bd3
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2018-03-21 18:11:59 +0100

    Swagger

--- Nexusat.AspNetCore.SampleRestApi/Startup.cs
+++ Nexusat.AspNetCore.SampleRestApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Nexusat.AspNetCore.SampleRestApi
 {
@@ -24,6 +25,11 @@ namespace Nexusat.AspNetCore.SampleRestApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Sample REST API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,11 @@ namespace Nexusat.AspNetCore.SampleRestApi
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample REST API V1");
+            });
         }
     }
 }
