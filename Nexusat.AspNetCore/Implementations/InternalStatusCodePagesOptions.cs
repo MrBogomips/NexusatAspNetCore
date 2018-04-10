c87f6cdfbb707f362041426d4aa97b7c86da3612
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2018-04-10 18:46:00 +0200

    MVC: StatusCode Management
    Generalized to all possible codes.

--- Nexusat.AspNetCore/Implementations/InternalStatusCodePagesOptions.cs
+++ Nexusat.AspNetCore/Implementations/InternalStatusCodePagesOptions.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Nexusat.AspNetCore.Models;
 using Nexusat.AspNetCore.Mvc.Formatters;
 
 namespace Nexusat.AspNetCore.Implementations
@@ -15,12 +16,21 @@ namespace Nexusat.AspNetCore.Implementations
         {
             HandleAsync = (ctx) =>
             {
-                if (ctx.HttpContext.Response.StatusCode == 404)
+                var services = ctx.HttpContext.RequestServices;
+                var executor = services.GetRequiredService<ApiResponseExecutor>();
+
+                var httpStatusCode = ctx.HttpContext.Response.StatusCode;
+                ApiResponse response = null; 
+                if ( httpStatusCode == 404)
                 {
-                    var services = ctx.HttpContext.RequestServices;
-                    var executor = services.GetRequiredService<ApiResponseExecutor>();
-                    executor.RenderResponse(ctx.HttpContext, new NotFoundResponse());
+                    response = new NotFoundResponse();
+                } else {
+                    response = new ApiResponse();
+                    response.Status.HttpCode = httpStatusCode;
+                    response.Status.Code = string.Format("{0}_{1}_UNMANAGED", StatusCode.KO, httpStatusCode);
                 }
+
+                executor.RenderResponse(ctx.HttpContext, response);
 
                 return Task.FromResult(0);
             };
