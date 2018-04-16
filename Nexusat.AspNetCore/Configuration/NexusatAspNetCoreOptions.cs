b33d61e6c7166cc03dbda90c800cd422111d51b8
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2018-04-16 16:09:51 +0200

    Pagination Configuration Extensions

--- Nexusat.AspNetCore/Configuration/NexusatAspNetCoreOptions.cs
+++ Nexusat.AspNetCore/Configuration/NexusatAspNetCoreOptions.cs
@@ -42,5 +42,39 @@ namespace Nexusat.AspNetCore.Configuration
         /// </summary>
         /// <value><c>true</c> if is runtime profilation enabled; otherwise, <c>false</c>.</value>
         public bool IsRuntimeProfilationEnabled { get; internal set; } = false;
+
+        /// <summary>
+        /// Gets or sets the name of the pagination page index used to parse the request querystring
+        /// </summary>
+        /// <value>The name of the pagination page index. Default is <code>pageIndex</code></value>
+        public string PaginationPageIndexName { get; internal set; } = "pageIndex";
+
+        /// <summary>
+        /// Gets or sets the name of the pagination page size used to parse the request querystring
+        /// </summary>
+        /// <value>The name of the pagination page size. Default is <code>pageSize</code></value>
+        public string PaginationPageSizeName { get; internal set; } = "pageSize";
+
+        /// <summary>
+        /// Gets or sets the size of the pagination default page.
+        /// </summary>
+        /// <value>The size of the pagination default page. Default is <code>10</code></value>
+        public int PaginationDefaultPageSize { get; internal set; } = 10;
+
+        /// <summary>
+        /// Gets or sets the size of the pagination default max page.
+        /// </summary>
+        /// <value>The size of the pagination default max page.</value>
+        public int PaginationDefaultMaxPageSize { get; internal set; } = 100;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this
+        /// <see cref="T:Nexusat.AspNetCore.Configuration.NexusatAspNetCoreOptions"/> pagination default bad request on
+        /// invalid request.
+        /// </summary>
+        /// <value><c>true</c> will block an page request too large generating a Bad Request response; otherwise, <c>false</c> will
+        /// imply the maximum value allowed for the request</value>
+        public bool PaginationDefaultBadRequestOnPageSizeOverflow { get; internal set; } = false;
+
     }
 }
