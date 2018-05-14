a0bebf74fbbe8b41ce46a167cf8ac8f833bab636
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2018-05-14 20:01:03 +0200

    Refactoring

--- Nexusat.AspNetCore/Models/Created.cs
+++ Nexusat.AspNetCore/Models/Created.cs
@@ -4,7 +4,7 @@ using StatusCodes = Microsoft.AspNetCore.Http.StatusCodes;
 namespace Nexusat.AspNetCore.Models
 {
 
-    /*
+	/*
      * protected IApiResponse Created(string description = null, string userDescription = null, string uri = null)
         => ApiResponse(Status201Created, FrameworkOptions.DefaultOkStatusCode, description, userDescription)
             .SetLocation(uri);
@@ -110,99 +110,5 @@ namespace Nexusat.AspNetCore.Models
                 StatusCode.CheckValidOkCodeOrThrow(statusCode);
             }
 		}
-	}
-
-	public static class Accepted
-	{
-		public const string DefaultStatusCode = CommonStatusCodes.OK_ + "ACCEPTED";
-		public const int HttpStatusCode = StatusCodes.Status202Accepted;
-
-		public class ResponseAtUri : ApiResponseWithLocation
-		{
-			public ResponseAtUri(string uri, string description = null, string userDescription = null)
-				:base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), uri) 
-			{ }
-			public ResponseAtUri(Uri uri, string description = null, string userDescription = null)
-				: base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), uri)
-            { }         
-			public ResponseAtUri(string statusCode, string uri, string description = null, string userDescription = null)
-				:base(new Status(HttpStatusCode, statusCode, description, userDescription), uri) 
-			{
-				StatusCode.CheckValidOkCodeOrThrow(statusCode);
-			}
-			public ResponseAtUri(string statusCode, Uri uri, string description = null, string userDescription = null)
-				: base(new Status(HttpStatusCode, statusCode, description, userDescription), uri) 
-			{ 
-				StatusCode.CheckValidOkCodeOrThrow(statusCode);
-			}
-		}
-        
-		public class ResponseAtAction: ApiResponseWithLocation
-		{
-			public ResponseAtAction(string actionName, string description = null, string userDescription = null, string controllerName = null, object routeValues = null, bool absoluteUrl = false) 
-				: base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), new ActionDescriptor(actionName, controllerName, routeValues, absoluteUrl)) { }
-
-			public ResponseAtAction(string statusCode, string actionName, string description = null, string userDescription = null, string controllerName = null, object routeValues = null, bool absoluteUrl = false) 
-				: base(new Status(HttpStatusCode, statusCode, description, userDescription), new ActionDescriptor(actionName, controllerName, routeValues, absoluteUrl)) 
-			{
-				StatusCode.CheckValidOkCodeOrThrow(statusCode);
-			}
-		}
-
-		public class ResponseAtRoute: ApiResponseWithLocation
-		{
-			public ResponseAtRoute(string routeName, string description = null, string userDescription = null, object routeValues = null, string protocol = null, string host = null) 
-				: base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), new RouteDescriptor(routeName, routeValues, protocol, host)) { }
-
-			public ResponseAtRoute(string statusCode, string routeName, string description = null, string userDescription = null, object routeValues = null, string protocol = null, string host = null)
-                : base(new Status(HttpStatusCode, statusCode, description, userDescription), new RouteDescriptor(routeName, routeValues, protocol, host)) 
-			{ 
-				StatusCode.CheckValidOkCodeOrThrow(statusCode);
-			}
-		}      
-	
-		public class ObjectAtUri<T> : ApiObjectResponseWithLocation<T>
-        {
-            public ObjectAtUri(string uri, T data = default(T), string description = null, string userDescription = null)
-                : base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), uri, data)
-            { }
-            public ObjectAtUri(Uri uri, T data = default(T), string description = null, string userDescription = null)
-                : base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), uri, data)
-            { }
-            public ObjectAtUri(string statusCode, string uri, T data = default(T), string description = null, string userDescription = null)
-                : base(new Status(HttpStatusCode, statusCode, description, userDescription), uri, data)
-            {
-                StatusCode.CheckValidOkCodeOrThrow(statusCode);
-            }
-            public ObjectAtUri(string statusCode, Uri uri, T data = default(T), string description = null, string userDescription = null)
-                : base(new Status(HttpStatusCode, statusCode, description, userDescription), uri, data)
-            {
-                StatusCode.CheckValidOkCodeOrThrow(statusCode);
-            }
-        }
-
-        public class ObjectAtAction<T> : ApiObjectResponseWithLocation<T>
-        {
-            public ObjectAtAction(string actionName, T data = default(T), string description = null, string userDescription = null, string controllerName = null, object routeValues = null, bool absoluteUrl = false)
-                : base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), new ActionDescriptor(actionName, controllerName, routeValues, absoluteUrl), data) { }
-
-			public ObjectAtAction(string statusCode, string actionName, T data = default(T), string description = null, string userDescription = null, string controllerName = null, object routeValues = null, bool absoluteUrl = false)
-                : base(new Status(HttpStatusCode, statusCode, description, userDescription), new ActionDescriptor(actionName, controllerName, routeValues, absoluteUrl), data)
-            {
-                StatusCode.CheckValidOkCodeOrThrow(statusCode);
-            }
-        }
-
-        public class ObjectAtRoute<T> : ApiObjectResponseWithLocation<T>
-        {
-            public ObjectAtRoute(string routeName, T data = default(T), string description = null, string userDescription = null, object routeValues = null, string protocol = null, string host = null)
-                : base(new Status(HttpStatusCode, DefaultStatusCode, description, userDescription), new RouteDescriptor(routeName, routeValues, protocol, host), data) { }
-
-			public ObjectAtRoute(string statusCode, string routeName, T data = default(T), string description = null, string userDescription = null, object routeValues = null, string protocol = null, string host = null)
-                : base(new Status(HttpStatusCode, statusCode, description, userDescription), new RouteDescriptor(routeName, routeValues, protocol, host), data)
-            {
-                StatusCode.CheckValidOkCodeOrThrow(statusCode);
-            }
-        }
 	}
 }
